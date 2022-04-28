80905013b68e901594fe310dae13f455ad965a2a
Bobby <bobbyskhs@gmail.com>
2022-04-27 21:18:14 -0400

    added windows downloader script

---
+++ dataset_downloader.bat
@@ -0,0 +1,8 @@
+@echo off
+
+echo "Downloading Dataset"
+wget "https:/\github.com\luciferreeves\Network-Intrusion-Detection\releases\download\dataset\2020.10.01.csv" "-q" "--show-progress" || echo "
+Wget not found!!! Please Install wget to run this script. 
+
+Wget GNU Page: https:/\www.gnu.org\software\wget\ 
+Wget for Windows: http:/\gnuwin32.sourceforge.net\packages\wget.htm"
