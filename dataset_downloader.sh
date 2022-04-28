400c5b5ec88dcd788131fd06e6bfd237659f7556
Bobby <bobbyskhs@gmail.com>
2022-04-27 21:16:35 -0400

    added error message to script

--- dataset_downloader.sh
+++ dataset_downloader.sh
@@ -1,3 +1,2 @@
 echo "Downloading Dataset"
-wget https://github.com/luciferreeves/Network-Intrusion-Detection/releases/download/dataset/2020.10.01.csv -q --show-progress
-echo "Done"
+wget https://github.com/luciferreeves/Network-Intrusion-Detection/releases/download/dataset/2020.10.01.csv -q --show-progress || echo "\nWget not found!!! Please Install wget to run this script. \n\nWget GNU Page: https://www.gnu.org/software/wget/ \nWget for Windows: http://gnuwin32.sourceforge.net/packages/wget.htm"
